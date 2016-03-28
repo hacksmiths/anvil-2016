c22f993f60e51d267e43661eedc738e18492c7f6
Terry Clark <i.am.terryclark@gmail.com>
2016-03-28 15:45:08 +0100

    Nearly there

--- gulpfile.js
+++ gulpfile.js
@@ -2,37 +2,41 @@
 // Plugins
 // --------------------------------------------------------------------
 
-var gulp        = require('gulp'),
-    sass        = require('gulp-sass'),
-    concat      = require('gulp-concat'),
-    watch       = require('gulp-watch'),
-    plumber     = require('gulp-plumber'),
-    minify_css  = require('gulp-minify-css'),
-    uglify      = require('gulp-uglify'),
-    sourcemaps  = require('gulp-sourcemaps'),
-    prefix      = require('gulp-autoprefixer'),
-    imagemin    = require('gulp-imagemin'),
-    jshint      = require('gulp-jshint'),
-    pngquant    = require('imagemin-pngquant'),
-    browseSync  = require('browser-sync');
+var gulp       = require('gulp'),
+    sass       = require('gulp-sass'),
+    concat     = require('gulp-concat'),
+    watch      = require('gulp-watch'),
+    plumber    = require('gulp-plumber'),
+    minify_css = require('gulp-minify-css'),
+    uglify     = require('gulp-uglify'),
+    //sourcemaps = require('gulp-sourcemaps'),
+    prefix     = require('gulp-autoprefixer'),
+    imagemin   = require('gulp-imagemin'),
+    jshint     = require('gulp-jshint'),
+    pngquant   = require('imagemin-pngquant'),
+    browseSync = require('browser-sync');
 
 // --------------------------------------------------------------------
 // Settings
 // --------------------------------------------------------------------
 
 var src = {
-    sass: "src/sass/**/*.scss",
-    js: "src/js/**/*.js",
-    img: "src/img/**/*"
+  html: "src/*.html",
+  sass: "src/sass/**/*.scss",
+  js: "src/js/**/*.js",
+  img: "src/img/**/*",
+  fonts: "src/fonts/*"
+
 };
 
 var output = {
-    js: "output/js",
-    css: "output/css",
-    img: "output/img/",
-    html: "output/**/*.html",
-    min_css: 'app.min.css',
-    min_js: 'app.min.js'
+  html: "output/",
+  js: "output/js",
+  css: "output/css",
+  img: "output/img/",
+  fonts: "output/fonts/",
+  min_css: 'app.min.css',
+  min_js: 'app.min.js'
 };
 
 // --------------------------------------------------------------------
@@ -40,32 +44,44 @@ var output = {
 // --------------------------------------------------------------------
 
 var onError = function(err) {
-    console.log(err);
-    this.emit('end');
+  console.log(err);
+  this.emit('end');
 };
+
+// --------------------------------------------------------------------
+// Task: HTML
+// --------------------------------------------------------------------
+
+gulp.task('html', function(){
+  return gulp.src(src.html)
+  .pipe(plumber({
+      errorHandler: onError
+  }))
+  .pipe(gulp.dest(output.html))
+});
 
 // --------------------------------------------------------------------
 // Task: SASS
 // --------------------------------------------------------------------
 
 gulp.task('sass', function() {
-    
-    return gulp.src(src.sass)
-        .pipe(plumber({
-            errorHandler: onError
-        }))
-        .pipe(sass({
-            includePaths: require('node-normalize-scss').includePaths
-        }))
-        .pipe(prefix('last 2 versions'))
-        .pipe(concat(output.min_css))
-        .pipe(gulp.dest(output.css))
-        .pipe(minify_css())
-        //.pipe(sourcemaps.init())
-        //.pipe(sourcemaps.write())
-        .pipe(gulp.dest(output.css))
-        .pipe(browseSync.reload({stream: true}));
-    
+
+  return gulp.src(src.sass)
+    .pipe(plumber({
+      errorHandler: onError
+    }))
+    .pipe(sass({
+      includePaths: require('node-reset-scss').includePath
+    }))
+    .pipe(prefix('last 2 versions'))
+    .pipe(concat(output.min_css))
+    .pipe(gulp.dest(output.css))
+    .pipe(minify_css())
+    .pipe(gulp.dest(output.css))
+    .pipe(browseSync.reload({
+      stream: true
+    }));
+
 });
 
 
@@ -74,18 +90,16 @@ gulp.task('sass', function() {
 // --------------------------------------------------------------------
 
 gulp.task('js', function() {
-    
-   return gulp.src(src.js)
-        .pipe(plumber({
-            errorHandler: onError
-        }))
-        .pipe(uglify())
-        .pipe(concat(output.min_js))
-//        .pipe(sourcemaps.init())
-//        .pipe(sourcemaps.write())
-        .pipe(gulp.dest(output.js))
-   
-})
+
+  return gulp.src(src.js)
+    .pipe(plumber({
+      errorHandler: onError
+    }))
+    .pipe(uglify())
+    .pipe(concat(output.min_js))
+    .pipe(gulp.dest(output.js))
+
+});
 
 // --------------------------------------------------------------------
 // Images
@@ -93,34 +107,47 @@ gulp.task('js', function() {
 
 gulp.task('img', function() {
 
-    return gulp.src(src.img)
-        .pipe(imagemin({
-            progressive: true,
-            svgoPlugins: [{removeViewBox: false}],
-            use: [pngquant()]
-        }))
-        .pipe(gulp.dest(output.img))
+  return gulp.src(src.img)
+    .pipe(imagemin({
+      progressive: true,
+      svgoPlugins: [{
+        removeViewBox: false
+      }],
+      use: [pngquant()]
+    }))
+    .pipe(gulp.dest(output.img))
 
-})
+});
+
+// --------------------------------------------------------------------
+// Task: Fonts
+// --------------------------------------------------------------------
+gulp.task('fonts', function(){
+  return gulp.src(src.fonts)
+  .pipe(plumber({
+      errorHandler: onError
+  }))
+  .pipe(gulp.dest(output.fonts))
+});
 
 // --------------------------------------------------------------------
 // Watch
 // --------------------------------------------------------------------
 
 gulp.task('watch', function() {
-    
-    browseSync.init({
-       server: './output' 
-    });
-    gulp.watch(src.js, ['js']);
-    gulp.watch(src.sass, ['sass']);
-    gulp.watch(src.img, ['img']);
-    gulp.watch(output.html).on('change', browseSync.reload)
-    
-})
+
+  browseSync.init({
+    server: './output'
+  });
+  gulp.watch(src.js, ['js']);
+  gulp.watch(src.sass, ['sass']);
+  gulp.watch(src.img, ['img']);
+  gulp.watch(output.html).on('change', browseSync.reload)
+
+});
 
 // --------------------------------------------------------------------
 // Default
 // --------------------------------------------------------------------
 
-gulp.task('default', ['watch', 'sass', 'js', 'img']);  
+gulp.task('default', ['watch', 'html', 'sass', 'js', 'img', 'fonts']);
--- src/js/commands.js
+++
@@ -1,9 +0,0 @@
-function run() {
-    return true;
-}
-
-function call_it(name) {
-    console.log(name);
-}
-
-call_it(name);
